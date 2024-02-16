4a485ebe1a239f1ade051e09b450786ac1aa3dc9
joel <joelbmolina@gmail.com>
2024-02-16 13:53:12 -0700

    queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,41 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node: Node<T> = { value: item };
 
-}
+        if (!this.tail) {
+            this.tail = this.head = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+    }
     deque(): T | undefined {
+        if (!this.head) return;
 
-}
+        const head = this.head;
+        this.head = head.next;
+        this.length--;
+
+        if (this.length === 0) this.tail = undefined;
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
