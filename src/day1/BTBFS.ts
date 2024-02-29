db8aa1524c2bfb057f742c63e2f63f792694e1bd
joel <joelbmolina@gmail.com>
2024-02-29 15:36:15 -0700

    breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,14 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q = [head];
 
+    while (q.length) {
+        const curr = q.shift()!;
+
+        if (curr?.value === needle) return true;
+
+        if (curr?.right) q.push(curr.right);
+        if (curr?.left) q.push(curr.left);
+    }
+
+    return false;
 }
